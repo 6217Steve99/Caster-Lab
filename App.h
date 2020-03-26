a803dce04d4f56ca39375f762e8f2b71e237817d
Zachary <zcy621799@163.com>
2020-03-26 20:38:29 +0800

    bindable and drawable system

--- App.h
+++ App.h
@@ -9,9 +9,11 @@ public:
 	App();
 	// master frame / message loop
 	int Go();
+	~App();
 private:
 	void DoFrame();
 private:
 	Window wnd;
 	CasterTimer timer;
+	std::vector<std::unique_ptr<class Box>> boxes;
 };
